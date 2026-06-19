6c8ba14c3beaf02037f4521310f7a7734b6b0dc2
wannasly <imamaev2007@gmail.com>
2026-06-19 22:08:11 +0700

    Fix correct react plugin import name

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/plugin-react' // ИСПРАВЛЕНО ТУТ
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gitsite/', // ТАК СТИЛИ И СКРИПТЫ НЕ ПОТЕРЯЮТСЯ НА GITHUB PAGES
+  base: '/gitsite/',
 })
